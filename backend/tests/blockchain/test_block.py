3125f231e1572ab1691811c8d218ecee490ffaca
MAC <mac@Keshis-MacBook-Air.local>
2023-09-02 11:39:55 +0100

    new

---
+++ backend/tests/blockchain/test_block.py
@@ -0,0 +1,40 @@
+import time
+
+from backend.blockchain.block import Block, GENESIS_DATA
+from backend.config import MINE_RATE, SECONDS
+
+def test_mine_block():
+    last_block = Block.genesis()
+    data = 'test-data'
+    block = Block.mine_block(last_block, data)
+
+    assert isinstance(block, Block)
+    assert block.data == data
+    assert block.last_hash == last_block.hash
+    assert block.hash[0:block.difficulty] == '0' * block.difficulty
+
+def test_genesis():
+    genesis = Block.genesis()
+
+    assert isinstance(genesis, Block)
+
+    for key, value in GENESIS_DATA.items():
+        getattr(genesis, key) == value
+
+
+def test_quickly_mined_block():
+    last_block = Block.mine_block(Block.genesis(), 'foo')
+    mined_block = Block.mine_block(last_block, 'bar')
+
+    assert mined_block.difficulty == last_block.difficulty + 1
+
+
+
+
+def test_slowly_mined_block():
+    last_block = Block.mine_block(Block.genesis(), 'foo')
+    time.sleep(MINE_RATE / SECONDS)
+    mined_block = Block.mine_block(last_block, 'bar')
+
+    assert mined_block.difficulty == last_block.difficulty - 1
+
